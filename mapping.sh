bce90401acccd258de9281716eba5e0d49b17fa3
iam2b <54424364+iam2b@users.noreply.github.com>
2019-08-23 16:01:29 +0900

    Update mapping.sh

--- mapping.sh
+++ mapping.sh
@@ -10,4 +10,5 @@ minimap2 -ax splice -uf -k14 /lustre7/home/lustre4/akimitsu-lab/database/genome/
 ./190730_1_siCTRL/siCTRL_1d_pass.fastq > ./190730_1_siCTRL/siCTRL_1d_pass.sam                  #.fastq > .sam
 
 samtools view -b -S siCTRL_1d_pass.sam > siCTRL_1d_pass.bam                                    #.sam > .bam
+samtools sort siCTRL_1d_pass.bam -o siCTRL_1d_pass_sorted.bam                                  # sort bam files
 samtools index siCTRL_1d_pass.bam                                                              #.bam index
