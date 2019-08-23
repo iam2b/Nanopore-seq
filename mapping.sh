b39e108b842a7e8be880a679d8872d4b9d0c396a
iam2b <54424364+iam2b@users.noreply.github.com>
2019-08-23 15:49:27 +0900

    Update mapping.sh
    mapping by minimap2
    format transfer and index by samtools

--- mapping.sh
+++ mapping.sh
@@ -6,5 +6,8 @@
 #$ -l s_vmem=64G
 #$ -l mem_req=64G
 
-minimap2 -ax splice -uf -k14 /lustre7/home/lustre4/akimitsu-lab/database/genome/hg38/hg38.fa #reference in .fa format
-./190730_1_siCTRL/siCTRL_1d_pass.fastq > ./190730_1_siCTRL/siCTRL_1d_pass.sam                #.fastq > .sam
+minimap2 -ax splice -uf -k14 /lustre7/home/lustre4/akimitsu-lab/database/genome/hg38/hg38.fa   #reference in .fa format
+./190730_1_siCTRL/siCTRL_1d_pass.fastq > ./190730_1_siCTRL/siCTRL_1d_pass.sam                  #.fastq > .sam
+
+samtools view -b -S siCTRL_1d_pass.sam > siCTRL_1d_pass.bam                                    #.sam > .bam
+samtools index siCTRL_1d_pass.bam                                                              #.bam index
